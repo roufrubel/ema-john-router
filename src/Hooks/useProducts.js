6881199042451f92586365ea1a6da29f64495924
roufrubel <rubel.ma2012@gmail.com>
2021-10-02 01:31:11 +0800

    added cart

---
+++ src/Hooks/useProducts.js
@@ -0,0 +1,14 @@
+const { useEffect } = require("react");
+const { useState } = require("react")
+
+const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+    return [products, setProducts];
+}
+
+export default useProducts;
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import useProducts from '../../Hooks/useProducts';
+import useCart from '../../Hooks/useCart';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const history = useHistory();
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        removeFromDb(key);
+    }
+
+    const handlePlaceOrder = () => {
+        history.push('/placeorder')
+        setCart([]);
+        clearTheCart();
+    }
+
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        handleRemove={handleRemove}
+                        product={product}></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <div>
+                        <button onClick={handlePlaceOrder} className="btn-regular">Place order</button>
+                    </div>
+                </Cart>
+            </div>
         </div>
     );
 };
